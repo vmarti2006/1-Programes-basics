8de31cdd973182e78694163310d853617f6639fc
Victor <victormarti2006@example.com>
2025-09-30 16:11:54 +0200

    Exercici 2

--- exercicis/exercici2/Program.cs
+++ exercicis/exercici2/Program.cs
@@ -8,6 +8,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        Console.WriteLine("Carrer:");
+        string carrer = Console.ReadLine();
+        Console.WriteLine("Numero:");
+        string numero = Console.ReadLine();
+        Console.WriteLine("codi postal:");
+        string codi_postal = Console.ReadLine();
+        Console.WriteLine("poblacio:");
+        string poblacio = Console.ReadLine();
+
+        Console.WriteLine($"Carrer:{carrer}, numero {numero}, {poblacio}  {codi_postal}");
+    }      
 }
