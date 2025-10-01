2faafb67fe2c18d2f6a1f6c4d1ded416c7f614e7
Victor <victormarti2006@example.com>
2025-10-01 15:14:47 +0200

    Exercici 7

--- exercicis/exercici7/Program.cs
+++ exercicis/exercici7/Program.cs
@@ -5,8 +5,26 @@ Calcula la mitjana i mostra el resultat per pantalla. */
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Diges 3 numeros per fer mitjana");
+
+        Console.Write("Numero 1: ");
+        string numer_1 = Console.ReadLine();
+        double numer1 = Convert.ToInt16(numer_1);
+
+        Console.Write("Numero 2: ");
+        string numer_2 = Console.ReadLine();
+        double numer2 = Convert.ToInt16(numer_2);
+
+        Console.Write("Numero 3: ");
+        string numer_3 = Console.ReadLine();
+        double numer3 = Convert.ToInt16(numer_3);
+
+        double total = numer1 + numer2 + numer3;
+
+        double mitjana = total/3;
+
+        Console.WriteLine($"La suma de {numer1},{numer2},{numer3} es {total} i la seva mitjana es {mitjana}");
     }
 }
