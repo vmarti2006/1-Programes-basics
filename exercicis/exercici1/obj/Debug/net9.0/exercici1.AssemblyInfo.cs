dcc2f40b2a68f52dbe66549487cd804d63618d7a
coral-planaguma <coralplanaguma@gmail.com>
2025-10-01 15:33:34 +0200

    exercici 16

--- exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
+++ exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033395b090265ceecdda678dd9c7d01c3b25de61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906fcc2f2334889717b4a65bf555ed14a849dbf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
