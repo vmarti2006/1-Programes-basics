d2b9b99af8beee8fe6b477973f53af3b015ebea6
Victor <victormarti2006@example.com>
2025-10-02 10:43:32 +0200

    Exercici 14

--- exercicis/exercici14/Program.cs
+++ exercicis/exercici14/Program.cs
@@ -5,6 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Paraula a escollir");
+        string paraula = Console.ReadLine();
+
+        char primera = paraula[0];
+        char ultima = paraula[paraula.Length - 1];
+        int indexMig = paraula.Length / 2;
+        char mig = paraula[indexMig];
+
+        Console.WriteLine($"Primera lletra: {primera}");
+        Console.WriteLine($"Ultima lletra: {ultima}");
+        Console.WriteLine($"Lletra del mig: {mig}");
     }
 }
